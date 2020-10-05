be09e6083ac3095fc3c9c07517766e1374be9627
CinnyLin <yc58sky@gmail.com>
2020-10-05 23:24:24 +0800

    upload poker log files

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ pd.options.display.max_columns = 25
 
 # ---- Set parameters ----
 # fd_data = os.path.join("..", "self", "5H1AI_logs")  # location of data relative to code base path
-fd_data = os.path.join("..", "Data", "5H1AI_logs")  # location of data relative to code base path
-fn_data = [x for x in os.listdir(fd_data) if x.find('sample_game') > -1]  # list of data file names (log files)
+# location of data relative to code base path
+fd_data = os.path.join("Data", "5H1AI_logs")
+fn_data = [x for x in os.listdir(fd_data) if x.find(
+    'sample_game') > -1]  # list of data file names (log files)
 
 
 # ---- Define functions -----
@@ -43,7 +45,8 @@ def meta_game_stats(games_f, print_f=True, long_print_f=False):
         if len(g.error_hands) > 0:
             games_with_error_hands.append(g_num)
             if long_print_f:
-                print("%d hands missing information for game %s" % (len(g.error_hands), g_num))
+                print("%d hands missing information for game %s" %
+                      (len(g.error_hands), g_num))
         if g.start_hand > 0:
             games_start_hand_not_zero.update({g_num: str(g.start_hand)})
         if g.total_hands < min_number_hands_played[1]:
@@ -53,19 +56,29 @@ def meta_game_stats(games_f, print_f=True, long_print_f=False):
         if len(g.missing_hands) > 0:
             games_missing_hands.update({g_num: g.missing_hands})
         if g.combine_player_diff is not None:
-            games_with_combined_mismatch_players.update({g_num: g.combine_player_diff})
+            games_with_combined_mismatch_players.update(
+                {g_num: g.combine_player_diff})
             if long_print_f:
-                print("%s combined mismatch players for game %s" % (g.combine_player_diff, g_num))
+                print("%s combined mismatch players for game %s" %
+                      (g.combine_player_diff, g_num))
 
     if print_f:
-        print("Number of games processed (game object instantiations): %d" % len(games_f))
-        print("Number of games with hand parse errors: %d" % len(games_with_file_parse_errors))
-        print("Number of games with hand calculation errors: %d" % len(games_with_error_hands))
-        print("Number of games with starting hand number > 0: %d" % len(games_start_hand_not_zero))
-        print("Min %d number of hands in game %s" % (min_number_hands_played[1], min_number_hands_played[0]))
-        print("Max %d number of hands in game %s" % (max_number_hands_played[1], max_number_hands_played[0]))
-        print("Number of games with hand number missing in sequence: %d" % len(games_missing_hands))
-        print("Number of combined games with mismatch players: %d" % len(games_with_combined_mismatch_players))
+        print("Number of games processed (game object instantiations): %d" %
+              len(games_f))
+        print("Number of games with hand parse errors: %d" %
+              len(games_with_file_parse_errors))
+        print("Number of games with hand calculation errors: %d" %
+              len(games_with_error_hands))
+        print("Number of games with starting hand number > 0: %d" %
+              len(games_start_hand_not_zero))
+        print("Min %d number of hands in game %s" %
+              (min_number_hands_played[1], min_number_hands_played[0]))
+        print("Max %d number of hands in game %s" %
+              (max_number_hands_played[1], max_number_hands_played[0]))
+        print("Number of games with hand number missing in sequence: %d" %
+              len(games_missing_hands))
+        print("Number of combined games with mismatch players: %d" %
+              len(games_with_combined_mismatch_players))
         # print missing beginning hands
     if long_print_f:
         if len(games_start_hand_not_zero) > 0:
@@ -130,9 +143,11 @@ del p_name
 def create_player_df(player_f):
     def calc_hand_shift_vars(df_ff, shift_var_name_ff):
         # check to make sure all hands in range are continguous (no missing hands in middle of string)
-        t_df_ff = df_ff[['hand', shift_var_name_ff]].sort_values(by='hand', ascending=True).reindex()
+        t_df_ff = df_ff[['hand', shift_var_name_ff]].sort_values(
+            by='hand', ascending=True).reindex()
         if all((df_ff['hand'] - df_ff['hand'].shift(1))[1:] == 1):
-            t_df_ff['prev_' + shift_var_name_ff] = t_df_ff[shift_var_name_ff].shift(1)
+            t_df_ff['prev_' +
+                    shift_var_name_ff] = t_df_ff[shift_var_name_ff].shift(1)
         else:
             t_df_ff['prev_' + shift_var_name_ff] = None
         return t_df_ff.drop(columns=[shift_var_name_ff])
@@ -156,10 +171,12 @@ def create_player_df(player_f):
             except KeyError:
                 pass
 
-    df_f = pd.DataFrame(data=t_records).sort_values(by=['game', 'hand'], ascending=True)
+    df_f = pd.DataFrame(data=t_records).sort_values(
+        by=['game', 'hand'], ascending=True)
 
     # temp df of shift variables
-    t_df_f = df_f.groupby('game').apply(calc_hand_shift_vars, shift_var_name_ff='outcome').reset_index()
+    t_df_f = df_f.groupby('game').apply(
+        calc_hand_shift_vars, shift_var_name_ff='outcome').reset_index()
     t_df_f.drop(columns=['level_1'], inplace=True)
     df_f = df_f.merge(t_df_f, how='left', on=['game', 'hand'])
 
@@ -175,9 +192,12 @@ def create_base_df_wrapper(players_f):
     # sanity check on games / hands
     print('\n\n===== Sanity check dataframe =====')
     print('Total number of games: %d' % df.game.nunique())
-    print('%d players in data set: %s' % (len(df.player.unique()), df.player.unique()))
-    print('Min number of hands per game: %d' % df.groupby('game').hand.nunique().min())
-    print('Max number of hands per game: %d' % df.groupby('game').hand.nunique().max())
+    print('%d players in data set: %s' %
+          (len(df.player.unique()), df.player.unique()))
+    print('Min number of hands per game: %d' %
+          df.groupby('game').hand.nunique().min())
+    print('Max number of hands per game: %d' %
+          df.groupby('game').hand.nunique().max())
     print('Min number of unique players in a hand across all games: %d' % df.groupby(
         ['game', 'hand']).player.nunique().min())
     print('Max number of unique players in a hand across all games: %d' % df.groupby(
@@ -198,8 +218,10 @@ def create_base_df_wrapper(players_f):
     # add additional features
     df['prev_outcome_loss'] = df.prev_outcome < 0
     df['preflop_fold'] = df['preflop_action'] == 'f'
-    df['relative_start_stack'] = df['start_stack'] / df.groupby(['game', 'hand'])['start_stack'].transform('max')
-    df['rank_start_stack'] = df.groupby(['game', 'hand'])['start_stack'].rank(method='max', ascending=False)
+    df['relative_start_stack'] = df['start_stack'] / \
+        df.groupby(['game', 'hand'])['start_stack'].transform('max')
+    df['rank_start_stack'] = df.groupby(['game', 'hand'])[
+        'start_stack'].rank(method='max', ascending=False)
     df['any_blind'] = df['small_blind'] | df['big_blind']
 
     if df.groupby(['game', 'hand']).rank_start_stack.max().min() != 6:
@@ -248,22 +270,26 @@ df_prop_test['player'] = t_df.index
 del t_df
 
 print("Partitioning samples on %s and success event = '%s'." % (
-sample_partition_binary_field, success_event_binary_field))
-print("Test statistic z = (p1 - p2)/stdev and p1 is %s is True" % (sample_partition_binary_field))
+    sample_partition_binary_field, success_event_binary_field))
+print("Test statistic z = (p1 - p2)/stdev and p1 is %s is True" %
+      (sample_partition_binary_field))
 print(df_prop_test)
 
 # --- Logistic regression to incorporate stack size and previous loss
 # df = create_base_df_wrapper(players)    ########
 
 # create training data
-X_col_name = ['any_blind', 'prev_outcome_loss', 'slansky_rank', 'rank_start_stack']     # chen_rank, slansky_rank, premium_hole
+X_col_name = ['any_blind', 'prev_outcome_loss', 'slansky_rank',
+              'rank_start_stack']     # chen_rank, slansky_rank, premium_hole
 y_col_name = ['preflop_fold']
-dummy_vars = {'player': 'Pluribus'}  # base field name: value to drop for identification; if value is None, then no dummies are dropped
+# base field name: value to drop for identification; if value is None, then no dummies are dropped
+dummy_vars = {'player': 'Pluribus'}
 add_const = False
 
 # process dummy vars
 if len(dummy_vars) > 0:
-    df_logistic = df[X_col_name + list(dummy_vars.keys()) + y_col_name].dropna().reindex()
+    df_logistic = df[X_col_name +
+                     list(dummy_vars.keys()) + y_col_name].dropna().reindex()
     for t_name, t_excl in dummy_vars.items():
         t_df_dummies = pd.get_dummies(df_logistic[t_name])
         if t_excl is not None:
@@ -271,18 +297,21 @@ if len(dummy_vars) > 0:
         else:
             t_cols_to_drop = [t_name]
         del t_excl, t_name
-        df_logistic = pd.concat([df_logistic, t_df_dummies], axis=1).drop(columns=t_cols_to_drop)
+        df_logistic = pd.concat([df_logistic, t_df_dummies], axis=1).drop(
+            columns=t_cols_to_drop)
         X_col_name = [x for x in df_logistic.columns if x != y_col_name[0]]
         del t_cols_to_drop, t_df_dummies
 else:
     df_logistic = df[X_col_name + y_col_name].dropna().reindex()
-print('Regression data frame has %d observations and %d variables (incl. dependent)' % df_logistic.shape)
+print('Regression data frame has %d observations and %d variables (incl. dependent)' %
+      df_logistic.shape)
 
 if add_const:
     sm_result = sm.Logit(endog=df_logistic[y_col_name],
                          exog=sm.add_constant(df_logistic[X_col_name]).astype(float)).fit()
 else:
-    sm_result = sm.Logit(endog=df_logistic[y_col_name], exog=df_logistic[X_col_name].astype(float)).fit()
+    sm_result = sm.Logit(
+        endog=df_logistic[y_col_name], exog=df_logistic[X_col_name].astype(float)).fit()
 print(sm_result.summary2())
 
 
@@ -297,7 +326,8 @@ def get_select_hands(df_f, true_cond_cols_f):
     # retrieve games and hands for sub-population
     select_hands_dict_f = dict()
     for t_g_num in t_df['game'].unique():
-        select_hands_dict_f.update({str(t_g_num): [str(x) for x in t_df.loc[t_df['game'] == t_g_num, 'hand'].unique()]})
+        select_hands_dict_f.update({str(t_g_num): [str(
+            x) for x in t_df.loc[t_df['game'] == t_g_num, 'hand'].unique()]})
     return select_hands_dict_f
 
 
@@ -306,18 +336,23 @@ for p in players:
     # excl_cond1 = df.small_blind  # less likely to fold if already have money in the pot
     # excl_cond2 = df.big_blind  # less likely to fold if already have money in the pot
     # excl_cond4 = df.premium_hole  # got lucky with good hole cards no one folds
-    t_select_hands_prev_loss = get_select_hands(df.loc[df.player == p.name], true_cond_cols_f={'prev_outcome_loss': True, 'small_blind': False, 'big_blind': False, 'premium_hole': False})
-    t_select_hands_no_prev_loss = get_select_hands(df.loc[df.player == p.name], true_cond_cols_f={'prev_outcome_loss': False, 'small_blind': False, 'big_blind': False, 'premium_hole': False})
+    t_select_hands_prev_loss = get_select_hands(df.loc[df.player == p.name], true_cond_cols_f={
+                                                'prev_outcome_loss': True, 'small_blind': False, 'big_blind': False, 'premium_hole': False})
+    t_select_hands_no_prev_loss = get_select_hands(df.loc[df.player == p.name], true_cond_cols_f={
+                                                   'prev_outcome_loss': False, 'small_blind': False, 'big_blind': False, 'premium_hole': False})
 
     _, n1_success, n1 = p.calc_looseness(t_select_hands_prev_loss)
     _, n2_success, n2 = p.calc_looseness(t_select_hands_no_prev_loss)
     t_p1, t_p2, t_z, t_p = prop_2samp_ind_large(n1_success, n2_success, n1, n2)
-    looseness_comp.update({p.name: {'p1': t_p1, 'n1': n1_success, 'p2': t_p2, 'n2': n2_success, 'z': t_z, 'pval': t_p}})
+    looseness_comp.update({p.name: {'p1': t_p1, 'n1': n1_success,
+                                    'p2': t_p2, 'n2': n2_success, 'z': t_z, 'pval': t_p}})
 
     del p, t_p1, t_p2, t_z, t_p, n1_success, n1, n2_success, n2, t_select_hands_no_prev_loss, t_select_hands_prev_loss
 
-print("Partitioning samples on %s and success event = '%s'." % ('prev_outcome_loss', 'voluntarily played hand (looseness)'))
-print("Test statistic z = (p1 - p2)/stdev and p1 is %s is True" % ('prev_outcome_loss'))
+print("Partitioning samples on %s and success event = '%s'." %
+      ('prev_outcome_loss', 'voluntarily played hand (looseness)'))
+print("Test statistic z = (p1 - p2)/stdev and p1 is %s is True" %
+      ('prev_outcome_loss'))
 print(pd.DataFrame.from_dict(looseness_comp, orient='index'))
 
 # import pickle
