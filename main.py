a0c8e5a74f20d3d350ae79554af3376504456a22
CinnyLin <yc58sky@gmail.com>
2020-10-05 23:35:03 +0800

    count total hands

--- main.py
+++ main.py
@@ -359,4 +359,5 @@ print(pd.DataFrame.from_dict(looseness_comp, orient='index'))
 # with open("python_hand_data.pickle", 'wb') as f:
 #     pickle.dump({'players': players, 'games': games, 'df': df}, f)
 
+# output transformed data to csv file
 df.to_csv('cinny/df.csv')
